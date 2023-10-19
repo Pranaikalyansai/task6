e91829bbdd998d4c9e6674b4b06c0407e0154b02
Pranaikalyansai <144125493+Pranaikalyansai@users.noreply.github.com>
2023-10-19 15:22:25 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,13 +17,13 @@ function Footer() {
           
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} Venkat</h3>
+          <h3>Copyright © {year} PRANAI </h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/Konathalavenkat"
+               
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -33,7 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://leetcode.com/Venkat_siva/"
+               
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="www.linkedin.com/in/venkat-konathala-953a7b262"
+               
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.hackerrank.com/k_venkatsiva"
+              
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
