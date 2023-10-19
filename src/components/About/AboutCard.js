039c194abbcc94e6b40de7f36c0c0428b14c24ce
Pranaikalyansai <144125493+Pranaikalyansai@users.noreply.github.com>
2023-10-19 15:44:38 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,8 +8,8 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">PRANAI KALYAN SAI</span>
-            from <span className="purple"> Anakapalle, India.</span>&nbsp;
+            Hi Everyone, I am <span className="purple"> PRANAI KALYAN SAI</span>
+            from <span className="purple"> DHARMAVARAM, India.</span>&nbsp;
             I am a third year student at VIT-AP University studying Computer Science and engineering with specilaization in CYBER SECURITY
             <br />
             <br />
