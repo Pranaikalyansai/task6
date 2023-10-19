6577e3702b861f146fb1834bf86f52261b4beb7d
Pranaikalyansai <144125493+Pranaikalyansai@users.noreply.github.com>
2023-10-19 15:45:48 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,7 +20,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name">PRANAI KALYAN SAI </strong>
+                <strong className="main-name"> PRANAI KALYAN SAI </strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
